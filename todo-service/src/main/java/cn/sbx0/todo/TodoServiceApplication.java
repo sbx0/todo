9bd5668e51bcb385a4fb3d3ec2b111896e1d8d62
sbx0 <782715416@qq.com>
2022-10-24 16:05:13 +0800

    init project

---
+++ todo-service/src/main/java/cn/sbx0/todo/TodoServiceApplication.java
@@ -0,0 +1,13 @@
+package cn.sbx0.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TodoServiceApplication.class, args);
+  }
+
+}
---
+++ todo-service/src/test/java/cn/sbx0/todo/TodoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.sbx0.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
