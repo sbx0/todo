f46a9727b7a97148fd017c3adc2bef229408d0f9
sbx0 <782715416@qq.com>
2022-10-24 15:05:02 +0800

    init auth-center

---
+++ auth-center/src/main/java/cn/sbx0/todo/Main.java
@@ -0,0 +1,12 @@
+package cn.sbx0.todo;
+
+/**
+ * @author sbx0
+ * @since ${DATE}
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
