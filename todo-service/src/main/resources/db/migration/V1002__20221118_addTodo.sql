a4caa74aa172a336c7e4992adc6ad38c61c3fbef
sbx0 <782715416@qq.com>
2022-11-30 17:25:30 +0800

    add todo task table sql

---
+++ todo-service/src/main/resources/db/migration/V1002__20221118_addTodo.sql
@@ -0,0 +1,11 @@
+create table tasks
+(
+    id          bigint unsigned                not null comment 'id',
+    task_name   varchar(128)                   not null comment 'task name',
+    task_remark varchar(256)                   null comment 'task remark',
+    task_status tinyint unsigned default 0     not null comment 'task status',
+    plan_time   datetime                       null comment 'plan time',
+    create_time datetime         default now() not null comment 'when create',
+    update_time datetime                       null on update now() comment 'when update',
+    constraint tasks_pk primary key (id)
+);
