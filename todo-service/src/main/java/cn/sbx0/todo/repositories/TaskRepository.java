f0db154f8be883c0a73191cc5f479b0211618ebe
sbx0 <782715416@qq.com>
2022-12-06 11:35:57 +0800

    use text blocks to improve code readability

--- todo-service/src/main/java/cn/sbx0/todo/repositories/TaskRepository.java
+++ todo-service/src/main/java/cn/sbx0/todo/repositories/TaskRepository.java
@@ -11,16 +11,27 @@ import org.springframework.data.jpa.repository.Query;
  */
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
 
-  @Modifying
-  @Query(value = "insert into tasks (task_name) values (:#{#entity.taskName})", nativeQuery = true)
-  void customSave(TaskEntity entity);
+  //language=MySQL
+  String CUSTOM_SAVE_SQL = """
+      insert into tasks (task_name)
+      values (:#{#entity.taskName})
+            """;
 
   @Modifying
-  @Query(value = "UPDATE tasks\n"
-      + "SET task_name   = :#{#entity.taskName}\n"
-      + ", task_status = :#{#entity.taskStatus}\n"
-      + ", plan_time   = :#{#entity.planTime}\n"
-      + ", task_remark = :#{#entity.taskRemark}\n"
-      + "WHERE id = :#{#entity.id}", nativeQuery = true)
+  @Query(value = CUSTOM_SAVE_SQL, nativeQuery = true)
+  void customSave(TaskEntity entity);
+
+  //language=MySQL
+  String CUSTOM_UPDATE_SQL = """
+      UPDATE tasks
+      SET task_name   = :#{#entity.taskName},
+          task_status = :#{#entity.taskStatus},
+          plan_time   = :#{#entity.planTime},
+          task_remark = :#{#entity.taskRemark}
+      WHERE id = :#{#entity.id}
+            """;
+
+  @Modifying
+  @Query(value = CUSTOM_UPDATE_SQL, nativeQuery = true)
   void customUpdate(TaskEntity entity);
 }
