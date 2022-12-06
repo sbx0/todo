09719fd70424aeb16d87539bc6f80aa5004bfaa4
sbx0 <782715416@qq.com>
2022-12-06 16:33:28 +0800

    the background color of the completed task will changed

--- todo-web/pages/index.js
+++ todo-web/pages/index.js
@@ -61,29 +61,30 @@ export default function Home() {
         <main className={styles.main}>
           <div className={styles.contentArea}>
             {list.map((one) =>
-                <div key={'taskInfo_' + one.id} className={styles.taskItemBody}>
+                <div key={'taskInfo_' + one.id}
+                     className={`${styles.taskItemBody} ${1 === one.taskStatus
+                         ? styles.taskItemBodyCompleted : ''}`}>
                   <div className={styles.taskCheckIconContainer}>
-                    {
-                      one?.taskStatus ?
-                          <svg fill="currentColor" width="20"
-                               height="20" viewBox="0 0 20 20"
-                               xmlns="http://www.w3.org/2000/svg"
-                               focusable="false"
-                               onClick={() => setTaskStatusUndo(one)}>
-                            <path
-                                d="M10 2a8 8 0 110 16 8 8 0 010-16zm0 1a7 7 0 100 14 7 7 0 000-14zm3.36 4.65c.17.17.2.44.06.63l-.06.07-4 4a.5.5 0 01-.64.07l-.07-.06-2-2a.5.5 0 01.63-.77l.07.06L9 11.3l3.65-3.65c.2-.2.51-.2.7 0z"
-                                fill="currentColor"></path>
-                          </svg>
-                          :
-                          <svg fill="currentColor" width="20"
-                               height="20" viewBox="0 0 20 20"
-                               xmlns="http://www.w3.org/2000/svg"
-                               focusable="false"
-                               onClick={() => setTaskStatusCompleted(one)}>
-                            <path
-                                d="M10 3a7 7 0 100 14 7 7 0 000-14zm-8 7a8 8 0 1116 0 8 8 0 01-16 0z"
-                                fill="currentColor"></path>
-                          </svg>
+                    {one?.taskStatus ?
+                        <svg fill="currentColor" width="20"
+                             height="20" viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg"
+                             focusable="false"
+                             onClick={() => setTaskStatusUndo(one)}>
+                          <path
+                              d="M10 2a8 8 0 110 16 8 8 0 010-16zm0 1a7 7 0 100 14 7 7 0 000-14zm3.36 4.65c.17.17.2.44.06.63l-.06.07-4 4a.5.5 0 01-.64.07l-.07-.06-2-2a.5.5 0 01.63-.77l.07.06L9 11.3l3.65-3.65c.2-.2.51-.2.7 0z"
+                              fill="currentColor"></path>
+                        </svg>
+                        :
+                        <svg fill="currentColor" width="20"
+                             height="20" viewBox="0 0 20 20"
+                             xmlns="http://www.w3.org/2000/svg"
+                             focusable="false"
+                             onClick={() => setTaskStatusCompleted(one)}>
+                          <path
+                              d="M10 3a7 7 0 100 14 7 7 0 000-14zm-8 7a8 8 0 1116 0 8 8 0 01-16 0z"
+                              fill="currentColor"></path>
+                        </svg>
                     }
                   </div>
                   <div className={styles.taskNameContainer}>
