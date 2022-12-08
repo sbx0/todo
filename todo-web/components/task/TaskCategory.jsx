63553e9339c5e7780f9fe7f01b6b3181aadd6633
sbx0 <782715416@qq.com>
2022-12-08 16:10:28 +0800

    add load animation

--- todo-web/components/task/TaskCategory.jsx
+++ todo-web/components/task/TaskCategory.jsx
@@ -6,29 +6,31 @@ export default function TaskCategory() {
   const [list, setList] = useState([]);
 
   useEffect(() => {
-    listApi({page: 1, pageSize: 20}, false).then((res) => {
+    listApi({page: 1, pageSize: 20}, true).then((res) => {
       setList(res.data)
     })
   }, []);
 
   return <div className={styles.categoryContainer}>
+    <div className={styles.categoryItem}>
+      <input id={'category_default'}
+             name={'category'}
+             type="radio"
+             defaultChecked
+             hidden/>
+      <label className={styles.categoryLabel}
+             htmlFor={'category_default'}>
+        Default
+      </label>
+    </div>
     {
       list.map((one, index) => {
         return <div key={one.id + one.categoryName}
                     className={styles.categoryItem}>
-          {
-            index === 0 ?
-                <input id={'category_' + one.id}
-                       name={'category'}
-                       type="radio"
-                       defaultChecked
-                       hidden/>
-                :
-                <input id={'category_' + one.id}
-                       name={'category'}
-                       type="radio"
-                       hidden/>
-          }
+          <input id={'category_' + one.id}
+                 name={'category'}
+                 type="radio"
+                 hidden/>
           <label className={styles.categoryLabel}
                  htmlFor={'category_' + one.id}>{one.categoryName}</label>
         </div>
