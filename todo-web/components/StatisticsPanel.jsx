6518143590f18e231c9e28d068810d5d7fa1b971
sbx0 <782715416@qq.com>
2022-12-15 10:55:06 +0800

    add task statistics panel

---
+++ todo-web/components/StatisticsPanel.jsx
@@ -0,0 +1,44 @@
+import styles from "./StatisticsPanel.module.css";
+import useFetch from "../hooks/useFetch";
+import {useEffect, useState} from "react";
+
+export default function StatisticsPanel({setLoading}) {
+    const {data, refresh} = useFetch({
+        method: 'GET',
+        url: '/api/task/statistics',
+        setLoading: setLoading
+    });
+    const [completed, setCompleted] = useState(0);
+    const [uncompleted, setUncompleted] = useState(0);
+
+    useEffect(() => {
+        if (data != null) {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].key === 'completed') {
+                    setCompleted(data[i].value);
+                } else if (data[i].key === 'uncompleted') {
+                    setUncompleted(data[i].value);
+                }
+            }
+        }
+    }, [data]);
+
+    return <div className={styles.container}>
+        <div className={styles.item}>
+            <div className={styles.itemTitle}>
+                Completed
+            </div>
+            <div className={`${styles.itemValue} ${styles.completed}`}>
+                {completed}
+            </div>
+        </div>
+        <div className={styles.item}>
+            <div className={styles.itemTitle}>
+                Uncompleted
+            </div>
+            <div className={styles.itemValue}>
+                {uncompleted}
+            </div>
+        </div>
+    </div>;
+}
--- todo-web/pages/setting.jsx
+++ todo-web/pages/setting.jsx
@@ -1,10 +1,16 @@
+import styles from '../styles/Setting.module.css';
+
 import NavigationBar from "../components/NavigationBar";
+import StatisticsPanel from "../components/StatisticsPanel";
+import {useState} from "react";
+import Loading from "../components/Loading";
 
 export default function Setting() {
-    return <>
-        <h1>
-            Setting
-        </h1>
+    const [loading, setLoading] = useState(false);
+
+    return <div className={styles.container}>
+        <StatisticsPanel setLoading={setLoading}/>
         <NavigationBar active={1}/>
-    </>
+        <Loading active={loading}></Loading>
+    </div>
 }
