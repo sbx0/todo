e031991c62c039cc28a03a168a39196e452e9100
sbx0 <782715416@qq.com>
2023-01-11 17:59:24 +0800

    turn back xAxis data

--- todo-service/src/main/java/cn/sbx0/todo/repositories/AssetRecordRepository.java
+++ todo-service/src/main/java/cn/sbx0/todo/repositories/AssetRecordRepository.java
@@ -14,7 +14,7 @@ public interface AssetRecordRepository extends JpaRepository<AssetRecord, Long> 
     @Query(value = """
             select distinct record_time
             from asset_records
-            order by record_time desc
+            order by record_time
             limit 1000
             """, nativeQuery = true)
     List<String> getRecentRecordTimeList();
