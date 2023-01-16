cde45ece987495707b7b297bf97d9ba8be158ca0
sbx0 <782715416@qq.com>
2023-01-16 13:45:39 +0800

    init data from server side

--- todo-web/pages/done.jsx
+++ todo-web/pages/done.jsx
@@ -1,13 +1,62 @@
 import NavigationBar from "../components/NavigationBar";
 import TaskList from "../components/TaskList";
 import Container from "../components/Container";
+import {callApi} from "../apis/taskApi";
+import {
+    ApiPrefix,
+    buildDefaultParamsForCategoryPaging,
+    buildDefaultParamsForTaskPaging,
+    buildDefaultParamsForTaskStatistics,
+    CategoryPaging,
+    GET,
+    POST,
+    TaskPaging,
+    TaskStatistics
+} from "../apis/apiPath";
 
-export default function Done() {
+export default ({initData, category, statistics}) => {
 
     return <Container>
-        <TaskList timeType={'update_time'}
-                  orderBy={'update_time'}
-                  taskStatus={1}/>
+        <TaskList initData={initData}
+                  category={category}
+                  statistics={statistics}
+                  taskStatus={1}
+                  timeType={'update_time'}
+                  orderBy={'update_time'}/>
         <NavigationBar active={1}/>
     </Container>
 }
+
+export async function getServerSideProps({req, query}) {
+    let categoryId = 0;
+    if (query.categoryId != null) {
+        categoryId = query.categoryId;
+    }
+
+    const result = await callApi({
+        method: POST,
+        url: ApiPrefix + req.headers.host + TaskPaging,
+        params: buildDefaultParamsForTaskPaging(categoryId)
+    });
+
+    const category = await callApi({
+        method: POST,
+        url: ApiPrefix + req.headers.host + CategoryPaging,
+        params: buildDefaultParamsForCategoryPaging()
+    });
+
+    const statistics = await callApi({
+        method: GET,
+        url: ApiPrefix + req.headers.host + TaskStatistics,
+        params: buildDefaultParamsForTaskStatistics(categoryId)
+    });
+
+    return {
+        props: {
+            initData: result.data,
+            category: category.data,
+            statistics: statistics.data
+        }
+    }
+}
+
