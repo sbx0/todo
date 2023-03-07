105f85efccb1ae71aa6d70f67c32118763e25b46
sbx0 <782715416@qq.com>
2023-03-07 17:21:39 +0800

    development env alert

--- todo-web/components/task/TaskInput.jsx
+++ todo-web/components/task/TaskInput.jsx
@@ -34,7 +34,7 @@ export default function TaskInput({categoryId, initData, setCategoryId, saveEven
                       clickEvent={clickEvent}/>
         <input type='text'
                id='taskInput'
-               placeholder='Input New Task'
+               placeholder={process.env.NODE_ENV === 'development' ? 'THIS IS DEV ENV!!!' : 'Input New Task'}
                className={styles.taskInput}
                value={newTask}
                onChange={(event) => setNewTask(event.target.value)}
