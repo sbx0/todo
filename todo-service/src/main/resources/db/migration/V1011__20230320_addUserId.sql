919e51936b29081762ee110956c795124cd40371
sbx0 <782715416@qq.com>
2023-03-20 15:57:30 +0800

    fix misspell

--- todo-service/src/main/resources/db/migration/V1011__20230320_addUserId.sql
+++ todo-service/src/main/resources/db/migration/V1011__20230320_addUserId.sql
@@ -2,10 +2,10 @@ alter table client_user
     add constraint client_user_pk
         unique (username);
 ALTER TABLE tasks
-    ADD user_id BIGINT UNSIGNED DEFAULT 1 NOT NULL COMMENT 'customUser id';
+    ADD user_id BIGINT UNSIGNED DEFAULT 1 NOT NULL COMMENT 'user id';
 ALTER TABLE categories
-    ADD user_id BIGINT UNSIGNED DEFAULT 1 NULL COMMENT 'customUser id';
+    ADD user_id BIGINT UNSIGNED DEFAULT 1 NULL COMMENT 'user id';
 ALTER TABLE asset_records
-    ADD user_id BIGINT UNSIGNED DEFAULT 1 NOT NULL COMMENT 'customUser id';
+    ADD user_id BIGINT UNSIGNED DEFAULT 1 NOT NULL COMMENT 'user id';
 ALTER TABLE asset_types
-    ADD user_id BIGINT UNSIGNED DEFAULT 1 NOT NULL COMMENT 'customUser id';
+    ADD user_id BIGINT UNSIGNED DEFAULT 1 NOT NULL COMMENT 'user id';
