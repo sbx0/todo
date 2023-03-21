a1c401f5c2c757df85219c8d5e114a3a21f0c100
sbx0 <782715416@qq.com>
2023-03-21 15:06:26 +0800

    add nprogress

--- todo-web/pages/_app.jsx
+++ todo-web/pages/_app.jsx
@@ -1,7 +1,26 @@
 import '../styles/globals.css'
+import '../styles/nprogress.css';
+import {useEffect} from "react";
+import {useRouter} from "next/router";
+import NProgress from "nprogress";
 
 function MyApp({Component, pageProps}) {
-  return <Component {...pageProps} />
+    const router = useRouter();
+
+    useEffect(() => {
+        router.events.on("routeChangeStart", () => {
+            NProgress.start();
+        });
+        router.events.on("routeChangeComplete", () => {
+            NProgress.done();
+        });
+        router.events.on("routeChangeError", () => {
+            NProgress.done();
+        });
+    }, []);
+
+
+    return <Component {...pageProps} />
 }
 
 export default MyApp
