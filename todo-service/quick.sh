a9c7ed595b3490f036b2ca806a8075eea4564a9b
sbx0 <782715416@qq.com>
2023-03-23 14:08:49 +0800

    sync script file from server

--- todo-service/quick.sh
+++ todo-service/quick.sh
@@ -5,16 +5,18 @@
 NAME="todo-service"
 
 log() {
- tail -F -n 250 logs/current.log
+ docker logs -f -n 250 todo-service-todo-service-1
+ #tail -F -n 250 logs/current.log
 }
 
 build() {
  docker build -t $NAME:v$(date "+%Y%m%d.%H%M%S") .
  docker build -t $NAME:latest .
+ up
 }
 
 up() {
- docker-compose -p $NAME up -d --build
+ docker compose -p $NAME up -d --build
 }
 
 case "$1" in
