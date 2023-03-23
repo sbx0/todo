aec8486bdfc78e4d038e1bdd84fc5c2a036cb6f3
sbx0 <782715416@qq.com>
2023-03-23 13:54:08 +0800

    chang config to export static html file

--- todo-web/next.config.js
+++ todo-web/next.config.js
@@ -3,17 +3,21 @@ const withPWA = require('next-pwa')({
     register: true,
     skipWaiting: true
 })
-const settings = {
-    output: 'standalone',
-    reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://' + (process.env.NODE_ENV === 'development' ? 'localhost:8888' : 'todo-service:9999') + '/:path*' // Proxy to Backend
-            }
-        ]
+module.exports = process.env.NODE_ENV === 'development' ?
+    {
+        output: 'standalone',
+        reactStrictMode: true,
+        async rewrites() {
+            return [
+                {
+                    source: '/api/:path*',
+                    destination: 'http://' + (process.env.NODE_ENV === 'development' ? 'localhost:8888' : 'todo-service:9999') + '/:path*' // Proxy to Backend
+                }
+            ]
+        }
     }
-}
-
-module.exports = process.env.NODE_ENV === 'development' ? settings : withPWA(settings);
+    :
+    withPWA({
+        output: 'export',
+        reactStrictMode: true
+    });
