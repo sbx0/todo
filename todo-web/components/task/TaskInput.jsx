f01bca5555585017546814cdc8b3de35c30dc0aa
sbx0 <782715416@qq.com>
2023-03-24 16:23:03 +0800

    optimize styles

--- todo-web/components/task/TaskInput.jsx
+++ todo-web/components/task/TaskInput.jsx
@@ -76,8 +76,8 @@ export default function TaskInput({saveEvent, clickEvent, setLoading}) {
         <TaskCategory clickEvent={categoryClickEvent}/>
         <input type='text'
                id='taskInput'
-               placeholder={process.env.NODE_ENV === 'development' ? 'THIS IS DEV ENV!!!' : 'Input New Task'}
-               className="taskInput"
+               placeholder={process.env.NODE_ENV === 'development' ? 'DEV VERSION WARNING' : '添加任务'}
+               className="input"
                value={newTask}
                onChange={(event) => setNewTask(event.target.value)}
                onKeyDown={event => {
@@ -92,13 +92,26 @@ export default function TaskInput({saveEvent, clickEvent, setLoading}) {
                       one={one}
                       change={changeTask}/>)}
         <style jsx>{`
-          .taskInput {
+          .input {
             width: 100%;
             height: 40px;
             font-size: 20px;
             display: block;
             margin: 10px auto;
+            padding: 0 10px;
             z-index: 9999;
+            background: #262a2d;
+            color: #fff;
+            outline: none;
+            border: 0;
+            border-bottom: 1px solid rgba(0, 143, 34, 0.51);
+            border-radius: 5px;
+            transition: border-bottom-color 500ms;
+          }
+
+          .input:focus {
+            padding: 1px 10px 0 10px;
+            border-bottom: 2px solid rgba(0, 215, 53, 0.51);
           }
         `}</style>
     </>;
