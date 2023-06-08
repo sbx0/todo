897488c7745c9b3f7d290cefb73c9ad3fdb9d806
sbx0 <782715416@qq.com>
2023-06-08 11:34:08 +0800

    refine components

---
+++ todo-web/components/beta/TaskDetail.jsx
@@ -0,0 +1,48 @@
+import styles from "./TaskDetail.module.css"
+import Button from "../basic/Button";
+
+export default function TaskDetail({current, setCurrent, changeTask}) {
+    return <>
+        <div className={`${styles.label}`}>{current?.categoryName}</div>
+        <div className={`${styles.textAreaDiv}`}>
+                        <textarea
+                            onKeyDown={(event) => {
+                                if (event.key === "Enter") {
+                                    event.preventDefault();
+                                }
+                            }}
+                            rows={1}
+                            value={current?.taskName}
+                            onChange={(event) => {
+                                setCurrent({
+                                    ...current,
+                                    taskName: event.target.value.trim()
+                                })
+                            }}
+                            placeholder={"请输入任务名称"}
+                            className={`${styles.textAreaNoBackground}`}/>
+        </div>
+        <div className={`${styles.label}`}>备注</div>
+        <div className={`${styles.textAreaDiv}`}>
+                        <textarea
+                            onKeyDown={(event) => {
+                                if (event.key === "Enter") {
+                                    event.preventDefault();
+                                }
+                            }}
+                            rows={3}
+                            placeholder={"在此处插入笔记"}
+                            value={current?.taskRemark}
+                            onChange={(event) => {
+                                setCurrent({
+                                    ...current,
+                                    taskRemark: event.target.value.trim()
+                                })
+                            }}
+                            className={`${styles.textAreaNoBackground}`}/>
+        </div>
+        <Button onClick={() => changeTask(current)}>
+            保存
+        </Button>
+    </>
+}
--- todo-web/pages/task/[param].jsx
+++ todo-web/pages/task/[param].jsx
@@ -1,15 +1,16 @@
 import {useEffect, useRef, useState} from "react";
-import styles from "../../styles/Beta.module.css"
+import styles from "../../styles/Tasks.module.css"
+
 import TaskItem from "../../components/beta/TaskItem";
 import Padding from "../../components/beta/Padding";
-import {POST, TaskPaging} from "../../apis/apiPath";
 import {callApi} from "../../apis/request";
+import {POST, TaskPaging} from "../../apis/apiPath";
 import NavBar from "../../components/beta/NavBar";
 import {useRouter} from "next/router";
 import Model from "../../components/beta/Model";
-import Button from "../../components/basic/Button";
+import TaskDetail from "../../components/beta/TaskDetail";
 
-export default function CategoryId() {
+export default function Tasks() {
     const router = useRouter();
     const centerRef = useRef(null);
     const [page, setPage] = useState(1);
@@ -228,47 +229,9 @@ export default function CategoryId() {
         </div>
         <Model show={modalShow}
                close={() => setModalShow(false)}>
-            <div className={`${styles.label}`}>{current?.categoryName}</div>
-            <div className={`${styles.textAreaDiv}`}>
-                        <textarea
-                            onKeyDown={(event) => {
-                                if (event.key === "Enter") {
-                                    event.preventDefault();
-                                }
-                            }}
-                            rows={1}
-                            value={current?.taskName}
-                            onChange={(event) => {
-                                setCurrent({
-                                    ...current,
-                                    taskName: event.target.value.trim()
-                                })
-                            }}
-                            placeholder={"请输入任务名称"}
-                            className={`${styles.textAreaNoBackground}`}/>
-            </div>
-            <div className={`${styles.label}`}>备注</div>
-            <div className={`${styles.textAreaDiv}`}>
-                        <textarea
-                            onKeyDown={(event) => {
-                                if (event.key === "Enter") {
-                                    event.preventDefault();
-                                }
-                            }}
-                            rows={3}
-                            placeholder={"在此处插入笔记"}
-                            value={current?.taskRemark}
-                            onChange={(event) => {
-                                setCurrent({
-                                    ...current,
-                                    taskRemark: event.target.value.trim()
-                                })
-                            }}
-                            className={`${styles.textAreaNoBackground}`}/>
-            </div>
-            <Button onClick={() => changeTask(current)}>
-                保存
-            </Button>
+            <TaskDetail current={current}
+                        setCurrent={setCurrent}
+                        changeTask={changeTask}/>
         </Model>
     </div>
 }
