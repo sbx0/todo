ba08870d71061cc4ad6e55b9c6539bddbaf3da52
sbx0 <782715416@qq.com>
2023-06-09 22:31:46 +0800

    rollback next.config.js

--- todo-web/next.config.js
+++ todo-web/next.config.js
@@ -3,15 +3,24 @@ const withPWA = require('next-pwa')({
     register: true,
     skipWaiting: true
 })
-module.exports = withPWA({
-    output: 'standalone',
-    reactStrictMode: true,
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: 'http://localhost:8888/:path*' // Proxy to Backend
-            }
-        ]
+module.exports = process.env.NODE_ENV === 'development' ?
+    {
+        output: 'standalone',
+        reactStrictMode: true,
+        async rewrites() {
+            return [
+                {
+                    source: '/api/:path*',
+                    destination: 'http://localhost:8888/:path*' // Proxy to Backend
+                }
+            ]
+        }
     }
-})
+    :
+    withPWA({
+        output: 'export',
+        reactStrictMode: true,
+        images: {
+            unoptimized: true
+        }
+    });
